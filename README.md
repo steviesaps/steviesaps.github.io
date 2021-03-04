8e61a09ee5382ddf3dee59c5955adbe7fd2cc194
Stevie S <79587359+steviesaps@users.noreply.github.com>
2021-03-04 15:39:12 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# steviesaps.github.io
-https://realpython.com/quizzes/python-csv/results/?t=eyJjIjo3LCJuIjo3LCJxIjozNCwic2lnIjoiempFM1k1MV8kZE5XUEg2IX4oaXNDUE9gdXc1WXVadyNSaXtnezFuKyIsInQiOjc2LCJ2IjozfQ==
+# aws_restart
---
+++ list_tuple_dictionary_solution.py
@@ -0,0 +1,38 @@
+# Lab 4: List, Tuple, Dictionary
+
+# Exercise 1: The List Data Type
+
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+# Exercise 2: The Tuple Data Type
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Exericise 3: The Dictionary Data Type
+
+myFavouriteFruitDictionary = {
+    "Adam" : "apple",
+    "Ben" : "banana",
+    "Penny" : "pineapple"
+    }
+print(myFavouriteFruitDictionary)
+print(type(myFavouriteFruitDictionary))
+
+print(myFavouriteFruitDictionary["Adam"])
+print(myFavouriteFruitDictionary["Ben"])
+print(myFavouriteFruitDictionary["Penny"])
